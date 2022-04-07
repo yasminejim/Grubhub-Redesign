129032a703c38ba5f053d140b22a91f63b659e73
Balexm95 <bria.mccoy95@gmail.com>
2022-04-07 11:20:24 -0400

    working on icon slider

--- grubhub_redesign/src/Components/IconSlider.jsx
+++ grubhub_redesign/src/Components/IconSlider.jsx
@@ -4,25 +4,37 @@ import {MdChevronLeft, MdChevronRight} from 'react-icons/md';
 const IconSlider = (props) => {
 
     const slides = [1,2,3,4,5,6,7,8];
+
+    const slideLeft=()=>{
+        let slider =document.getElementById("slider");
+        slider.scrollLeft = slider.scrollLeft+500
+    }
+
+    const slideRight=()=>{
+        let slider =document.getElementById("slider");
+        slider.scrollLeft = slider.scrollLeft-500
+    }
   return (
       <>
     <div className='icon-slider-container'>
         <div id="icon-slider">
-           <MdChevronLeft size ={40} className="slider-icon left"/>
+           <MdChevronLeft size ={40} className="slider-icon left" onClick={slideLeft}/>
            
            <div id="slider">
                {
                slides.map((slide,index) => {
                    return(
                        <div className= "slider-card">
+                           <div className ="slider-card-image">
 
+                           </div>
                        </div>
                    )
                })
             }
            </div>
            
-           <MdChevronRight size ={40} className="slider-icon right"/>
+           <MdChevronRight size ={40} className="slider-icon right" onClick={slideRight}/>
         </div>
         </div>
         </>
